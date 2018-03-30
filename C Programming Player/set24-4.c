d4c1b249027f3d5688ab2c03469eba796569bc8f
15ec023 <priya98.pb@gmail.com>
2018-03-30 16:39:31 +0530

    Create set24-4.c

---
+++ C Programming Player/set24-4.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+  int x,y,arr[10][10],i,j,sum=0,sum1=0;
+  scanf("%d",&x);//no of rows
+  scanf("%d",&y);//no of columns
+  for(i=0;i<x;i++)
+  {
+      for(j=0;j<y;j++)
+      {
+          scanf("%d",&arr[i][j]);
+          
+      }
+  }
+  for(i=0;i<x;i++)
+  {
+      for(j=0;j<y;j++)
+      {
+          if(i==j)
+           sum=sum+arr[i][j];
+          else
+           sum1=sum1+arr[i][j]);
+          
+      }
+  }
+  int p=sum*sum1;
+  printf("\n%d",p);
+}
